c90a243bc974f392c7ff1df7669476f57d200c74
Michael-Studivan <studivanms@gmail.com>
2024-03-04 09:55:41 -0500

    copy numbers

--- symbionts/urban_symbionts.R
+++ symbionts/urban_symbionts.R
@@ -25,7 +25,7 @@ Plates # Plates 1-8 and 11-16 have Sample 12/NTC split across plates, so results
 # Run the steponeR program following your plate target labels
 Urban_Out <- steponeR(files=Plates, target.ratios=c("B.A", "C.A", "D.A", "A.B", "C.B", "D.B", "A.C", "B.C", "D.C", "A.D", "B.D", "C.D"), 
                       fluor.norm=list(A=0, B=0, C=0, D=0),
-                      copy.number=list(A=1, B=1, C=1, D=1), # look up copy numbers
+                      copy.number=list(A=9, B=1, C=23, D=3), 
                       ploidy=list(A=1, B=1, C=1, D=1),
                       extract=list(A=0.813, B=0.813, C= 0.813, D=0.813))
 
